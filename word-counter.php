601660f4fe58e373dc203af9ae1a2994153a20a9
yungshun317 <seraph317@gmail.com>
2023-03-16 07:47:06 +0800

    [FEAT] Instantiate the `Word_Counter` class

--- word-counter.php
+++ word-counter.php
@@ -25,8 +25,37 @@ if ( ! class_exists( 'Word_Counter' ) ) {
 
         function settings() {
             add_settings_section( 'word_counter_section', null, null, 'word_counter' );
-            add_settings_field( 'word_counter_location', 'Display Location', array( $this. 'location-html' ), 'word_counter', 'word_counter_section' );
+            add_settings_field( 'word_counter_location', 'Display Location', array( $this, 'location_html' ), 'word_counter', 'word_counter_section' );
             register_setting( 'word_counter_group', 'word-counter-location', array( 'sanitize_callback' => 'sanitize_text_field', 'default' => '0' ) );
         }
+
+        function location_html() {
+            ?>
+            <select name="word_counter_location">
+                <option value="0">Beginning of Post</option>
+                <option value="1">End of Post</option>
+            </select>
+            <?php
+        }
+
+        function admin_page() {
+            add_options_page( 'Word Counter Settings', 'Word Counter', 'manage_options', 'word_counter_settings_page', array( $this, 'settings_html' ) );
+        }
+
+        function settings_html() {
+            ?>
+            <div class="wrap">
+                <h1>Word Counter Settings</h1>
+                <form action="options.php" method="POST"></form>
+                <?php
+                    settings_fields('word_counter_group' );
+                    do_settings_sections( 'word_counter_section' );
+                    submit_button();
+                ?>
+            </div>
+            <?php
+        }
     }
+
+    $word_counter = new Word_Counter();
 }
